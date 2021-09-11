e93136be6c606fc9b54aebfb93afaa9770a6adb5
Jay <jasonkleitz@gmail.com>
2021-09-11 12:33:05 -0400

    Remove duplicate InputActions script
    Removed the duplicate script

--- Assets/Liminality/Scripts/InputActions.cs
+++ Assets/Liminality/Scripts/InputActions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/InputActions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Liminality/Modules/InputActions.inputactions'
 
 using System;
 using System.Collections;
