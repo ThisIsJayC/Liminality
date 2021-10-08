b2c6255d382c6c441ca311b1ad2d136d9e0d30db
AegirTheLucky <90541036+AegirTheLucky@users.noreply.github.com>
2021-10-08 13:26:24 -0400

    Seperated Controls

--- Assets/InputActions.cs
+++ Assets/InputActions.cs
@@ -15,7 +15,7 @@ public class @InputActions : IInputActionCollection, IDisposable
     ""name"": ""InputActions"",
     ""maps"": [
         {
-            ""name"": ""Player"",
+            ""name"": ""Player (Gamepad)"",
             ""id"": ""4b3c270a-fb3b-45f4-b8d0-982369e1227d"",
             ""actions"": [
                 {
@@ -240,16 +240,150 @@ public class @InputActions : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": false
                 }
             ]
+        },
+        {
+            ""name"": ""Player (KVM)"",
+            ""id"": ""e690057b-97c4-4fab-a95c-912e2531e723"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""2446e137-a3c2-4cfb-8eec-fb6cff8b0f43"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""4c6a8228-2fb3-4bad-99cb-cd2ed02f3e94"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
+                },
+                {
+                    ""name"": ""Aim"",
+                    ""type"": ""Value"",
+                    ""id"": ""0ccc675f-81b9-4703-865b-de8cb2cf7103"",
+                    ""expectedControlType"": ""Analog"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Melee"",
+                    ""type"": ""Button"",
+                    ""id"": ""ea0cd1a8-c439-4244-b3d5-10d15ed68da7"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""63e9a329-0812-4e29-acce-2ebf761f6a78"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""2b99ca10-8d67-499b-8807-727ef490f567"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""4827f0c6-4d6a-47fe-b967-ac7cfdf350a8"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""9e8391e7-f734-44a7-a649-d8d0ec42f5e3"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""b3969fea-5d67-488d-96af-cdae302374bb"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9e8aa18d-cd82-42da-8157-845add775dba"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""aa8c257a-4a43-4872-92be-e83e7fe56f46"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Aim"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3a7e616a-bdc9-4cee-b6d2-86462f5fc279"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Melee"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": []
 }");
-        // Player
-        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
-        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
-        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
-        m_Player_Aim = m_Player.FindAction("Aim", throwIfNotFound: true);
-        m_Player_Melee = m_Player.FindAction("Melee", throwIfNotFound: true);
+        // Player (Gamepad)
+        m_PlayerGamepad = asset.FindActionMap("Player (Gamepad)", throwIfNotFound: true);
+        m_PlayerGamepad_Movement = m_PlayerGamepad.FindAction("Movement", throwIfNotFound: true);
+        m_PlayerGamepad_Jump = m_PlayerGamepad.FindAction("Jump", throwIfNotFound: true);
+        m_PlayerGamepad_Aim = m_PlayerGamepad.FindAction("Aim", throwIfNotFound: true);
+        m_PlayerGamepad_Melee = m_PlayerGamepad.FindAction("Melee", throwIfNotFound: true);
+        // Player (KVM)
+        m_PlayerKVM = asset.FindActionMap("Player (KVM)", throwIfNotFound: true);
+        m_PlayerKVM_Movement = m_PlayerKVM.FindAction("Movement", throwIfNotFound: true);
+        m_PlayerKVM_Jump = m_PlayerKVM.FindAction("Jump", throwIfNotFound: true);
+        m_PlayerKVM_Aim = m_PlayerKVM.FindAction("Aim", throwIfNotFound: true);
+        m_PlayerKVM_Melee = m_PlayerKVM.FindAction("Melee", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -296,44 +430,44 @@ public class @InputActions : IInputActionCollection, IDisposable
         asset.Disable();
     }
 
-    // Player
-    private readonly InputActionMap m_Player;
-    private IPlayerActions m_PlayerActionsCallbackInterface;
-    private readonly InputAction m_Player_Movement;
-    private readonly InputAction m_Player_Jump;
-    private readonly InputAction m_Player_Aim;
-    private readonly InputAction m_Player_Melee;
-    public struct PlayerActions
+    // Player (Gamepad)
+    private readonly InputActionMap m_PlayerGamepad;
+    private IPlayerGamepadActions m_PlayerGamepadActionsCallbackInterface;
+    private readonly InputAction m_PlayerGamepad_Movement;
+    private readonly InputAction m_PlayerGamepad_Jump;
+    private readonly InputAction m_PlayerGamepad_Aim;
+    private readonly InputAction m_PlayerGamepad_Melee;
+    public struct PlayerGamepadActions
     {
         private @InputActions m_Wrapper;
-        public PlayerActions(@InputActions wrapper) { m_Wrapper = wrapper; }
-        public InputAction @Movement => m_Wrapper.m_Player_Movement;
-        public InputAction @Jump => m_Wrapper.m_Player_Jump;
-        public InputAction @Aim => m_Wrapper.m_Player_Aim;
-        public InputAction @Melee => m_Wrapper.m_Player_Melee;
-        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public PlayerGamepadActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_PlayerGamepad_Movement;
+        public InputAction @Jump => m_Wrapper.m_PlayerGamepad_Jump;
+        public InputAction @Aim => m_Wrapper.m_PlayerGamepad_Aim;
+        public InputAction @Melee => m_Wrapper.m_PlayerGamepad_Melee;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerGamepad; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
         public bool enabled => Get().enabled;
-        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
-        public void SetCallbacks(IPlayerActions instance)
+        public static implicit operator InputActionMap(PlayerGamepadActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerGamepadActions instance)
         {
-            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            if (m_Wrapper.m_PlayerGamepadActionsCallbackInterface != null)
             {
-                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
-                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
-                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
-                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
-                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
-                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
-                @Aim.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAim;
-                @Aim.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAim;
-                @Aim.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAim;
-                @Melee.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMelee;
-                @Melee.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMelee;
-                @Melee.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMelee;
+                @Movement.started -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnJump;
+                @Aim.started -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnAim;
+                @Aim.performed -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnAim;
+                @Aim.canceled -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnAim;
+                @Melee.started -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnMelee;
+                @Melee.performed -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnMelee;
+                @Melee.canceled -= m_Wrapper.m_PlayerGamepadActionsCallbackInterface.OnMelee;
             }
-            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            m_Wrapper.m_PlayerGamepadActionsCallbackInterface = instance;
             if (instance != null)
             {
                 @Movement.started += instance.OnMovement;
@@ -351,8 +485,72 @@ public class @InputActions : IInputActionCollection, IDisposable
             }
         }
     }
-    public PlayerActions @Player => new PlayerActions(this);
-    public interface IPlayerActions
+    public PlayerGamepadActions @PlayerGamepad => new PlayerGamepadActions(this);
+
+    // Player (KVM)
+    private readonly InputActionMap m_PlayerKVM;
+    private IPlayerKVMActions m_PlayerKVMActionsCallbackInterface;
+    private readonly InputAction m_PlayerKVM_Movement;
+    private readonly InputAction m_PlayerKVM_Jump;
+    private readonly InputAction m_PlayerKVM_Aim;
+    private readonly InputAction m_PlayerKVM_Melee;
+    public struct PlayerKVMActions
+    {
+        private @InputActions m_Wrapper;
+        public PlayerKVMActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_PlayerKVM_Movement;
+        public InputAction @Jump => m_Wrapper.m_PlayerKVM_Jump;
+        public InputAction @Aim => m_Wrapper.m_PlayerKVM_Aim;
+        public InputAction @Melee => m_Wrapper.m_PlayerKVM_Melee;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerKVM; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerKVMActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerKVMActions instance)
+        {
+            if (m_Wrapper.m_PlayerKVMActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnJump;
+                @Aim.started -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnAim;
+                @Aim.performed -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnAim;
+                @Aim.canceled -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnAim;
+                @Melee.started -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnMelee;
+                @Melee.performed -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnMelee;
+                @Melee.canceled -= m_Wrapper.m_PlayerKVMActionsCallbackInterface.OnMelee;
+            }
+            m_Wrapper.m_PlayerKVMActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Aim.started += instance.OnAim;
+                @Aim.performed += instance.OnAim;
+                @Aim.canceled += instance.OnAim;
+                @Melee.started += instance.OnMelee;
+                @Melee.performed += instance.OnMelee;
+                @Melee.canceled += instance.OnMelee;
+            }
+        }
+    }
+    public PlayerKVMActions @PlayerKVM => new PlayerKVMActions(this);
+    public interface IPlayerGamepadActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnAim(InputAction.CallbackContext context);
+        void OnMelee(InputAction.CallbackContext context);
+    }
+    public interface IPlayerKVMActions
     {
         void OnMovement(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
